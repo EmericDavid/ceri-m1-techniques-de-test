13a76628d594cad55abe361fdb87a335b65fa2e4
Emeric DAVID <emericd17@gmail.com>
2024-11-18 16:56:44 +0100

    Push doc V8

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
@@ -8,13 +8,13 @@ package fr.univavignon.pokedex.api;
 public interface IPokedexFactory {
 
 	/**
-	 * Creates a new pokedex instance using the given 
-	 * <tt>metadataProvider</tt> and <tt>pokemonFactory</tt>. 
+	 * Creates a new pokedex instance using the given
+	 * <code>metadataProvider</code> and <code>pokemonFactory</code>.
 	 * 
 	 * @param metadataProvider Metadata provider the created pokedex will use.
-	 * @param pokemonFactory Pokemon factory the created pokedex will use.
+	 * @param pokemonFactory   Pokemon factory the created pokedex will use.
 	 * @return Created pokedex instance.
 	 */
 	IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory);
-	
+
 }
